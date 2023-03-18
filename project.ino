6a562fb38294f360ff4d87dc61feb14e026e87fe
Landon <102832670+LostBitset@users.noreply.github.com>
2023-03-18 13:06:05 -0400

    Odd/Even demo and updating

--- project.ino
+++ project.ino
@@ -25,6 +25,7 @@ char sevseg_anode_table[] = {
   0b11100000, // 7
   0b11111110, // 8
   0b11110110, // 9
+  0b00000000, // space
   0b01101100, // mojibake
   0b11101110, // A
   0b00111110, // B
@@ -46,18 +47,18 @@ char sevseg_anode_table[] = {
   0b01001110, // Y
 };
 
+#define SEVSEG_SPACE 10
+
 char sevseg_latin[] = {
 // A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
-  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 10, 21, 10, 22, 23, 24, 10, 25, 10, 26, 27, 10, 10, 10, 28, 10
+  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 11, 22, 11, 23, 24, 25, 11, 26, 11, 27, 28, 11, 11, 11, 29, 11
 };
 
-#define SEVSEG_LTN(X) sevseg_latin[X - 'A']
+#define SEVSEG_LTN(X) sevseg_latin[(X) - 'A']
 
 unsigned int sevseg_num_digits = sizeof(sevseg_anode_table)/sizeof(sevseg_anode_table[0]);
 
-unsigned int sevseg_active = 0;
-
-#define SEVSEG_REFRESH_TIME_MS 20
+#define SEVSEG_REFRESH_TIME_MS 12
 
 #define SEVSEG_DIGIT_TIME_MS (SEVSEG_REFRESH_TIME_MS/SEVSEG_N_DIGITS)
 
@@ -96,6 +97,11 @@ void setup() {
 // Show a digit on the Four-Digit Seven-Segment Display
 // which_digit is 0, 1, 2, or 3
 void sevseg_show_digit(int digit, int which_digit) {
+  // First turn everything off to avoid ghosting
+  digitalWrite(PIN_SEVSEG_CATH_1, HIGH);
+  digitalWrite(PIN_SEVSEG_CATH_2, HIGH);
+  digitalWrite(PIN_SEVSEG_CATH_3, HIGH);
+  digitalWrite(PIN_SEVSEG_CATH_4, HIGH);
   // Set anodes high to enable
   char anode_mask = sevseg_anode_table[digit];
   digitalWrite(PIN_SEVSEG_ANOD_A, (anode_mask & (1 << 7)) ? HIGH : LOW);
@@ -113,13 +119,65 @@ void sevseg_show_digit(int digit, int which_digit) {
   digitalWrite(PIN_SEVSEG_CATH_4, (which_digit == 3) ? LOW : HIGH);
 }
 
-void sevseg_show(int text[]) {
+unsigned int sevseg_show(int text[]) {
   unsigned int cycle_index = (millis() / SEVSEG_DIGIT_TIME_MS) % SEVSEG_N_DIGITS;
   sevseg_show_digit(text[cycle_index], cycle_index);
+  return cycle_index;
 }
+
+unsigned int sevseg_show_with_dp(int text[], int dp_pos) {
+  unsigned int cycle_index = sevseg_show(text);
+  if (cycle_index == dp_pos) {
+    digitalWrite(PIN_SEVSEG_ANOD_H, HIGH);
+  }
+  return cycle_index;
+}
+
+int sevseg_text[] = {SEVSEG_SPACE, SEVSEG_LTN('N'), SEVSEG_LTN('O'), SEVSEG_SPACE};
+
+unsigned int sevseg_dp_pos = 0;
+
+void write_sevseg_text() {
+  sevseg_show_with_dp(sevseg_text, sevseg_dp_pos);
+}
+
+void update_sevseg_text(int text[]) {
+  for (int i = 0; i < SEVSEG_N_DIGITS; i++) {
+    sevseg_text[i] = text[i];
+  }
+  sevseg_dp_pos += 1;
+  sevseg_dp_pos %= SEVSEG_N_DIGITS;
+}
+
+int number = 0;
+bool ready = false;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int test_text[] = {SEVSEG_LTN('H'), SEVSEG_LTN('E'), SEVSEG_LTN('L'), SEVSEG_LTN('O')};
-  sevseg_show(test_text);
+  write_sevseg_text();
+  // Update the text
+  unsigned int time = millis() % 1000;
+  if (time < 5) {
+    if (ready) {
+      ready = false;
+      number += 1;
+      number %= 50;
+      boolean is_even = (number % 2) == 0;
+      int text[] = {
+        number / 10,
+        number % 10,
+        SEVSEG_SPACE,
+        SEVSEG_LTN(is_even ? 'E' : 'O'),
+      };
+      if (number == 42) {
+        text[0] = SEVSEG_LTN('A');
+        text[1] = SEVSEG_LTN('N');
+        text[2] = 5;
+        text[3] = SEVSEG_LTN('R');
+      }
+      update_sevseg_text(text);
+    }
+  } else {
+    ready = true;
+  }
 }
